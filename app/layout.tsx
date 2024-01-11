e9e756b1792d433aac3a4c932b0819b47672e775
Redi Ibra <r.ibra@teamsystem.com>
2024-01-11 14:52:14 +0100

    pushing project

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from "react";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import { orbitron, exo2 } from "./fonts";
+
+interface LaoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: LaoutProps) {
+  return (
+    <html lang="en" className={`${exo2.variable} ${orbitron.variable}`}>
+      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
+        <header>
+          <NavBar />
+        </header>
+        <main className="grow py-3">{children}</main>
+        <footer className="border-t py-3 text-center text-slate-500 text-xs ">
+          Game data and images courtesy of
+          <a
+            className="text-orange-800 hover:un"
+            href="https://rawg.io/"
+            target="_blank"
+          >
+            &nbsp;RAWG
+          </a>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/reviews/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Heading from "@/components/Heading";
+
+export default function StardewValleyPage() {
+  return (
+    <>
+      <Heading>Reviews</Heading>
+      <ul className="flex flex-col gap-3">
+        <li className="bg-white rounded border shadow w-80 hover:shadow-xl">
+          <Link href="/reviews/hollow-knight">
+            <img
+              src="/images/hollow-knight.jpg"
+              alt=""
+              width="320"
+              height="180"
+              className="mb-2 rounded-t"
+            ></img>
+            <h2 className="font-semibold font-orbitron py-1 text-center">
+              Hollow Knight
+            </h2>
+          </Link>
+        </li>
+        <li className="bg-white rounded border shadow w-80 hover:shadow-xl">
+          <Link href="/reviews/stardew-valley">
+            <img
+              src="/images/stardew-valley.jpg"
+              alt=""
+              width="320"
+              height="180"
+              className="mb-2 rounded"
+            ></img>
+            <h2 className="font-semibold font-orbitron py-1 text-center">
+              Stardew Valley
+            </h2>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
